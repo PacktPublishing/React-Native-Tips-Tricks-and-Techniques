821a681ac8cf299bc5fc134e089921258158907c
Pavlos Vinieratos <pvinis@gmail.com>
2018-12-25 21:30:24 +0100

    use it

---
+++ PlaygroundApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Calculator from './containers/Calculator'
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Calculator />
+        )
+    }
+}
