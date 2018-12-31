1521efa6de193f77d9f46f5a16cb161b8da57af6
Pavlos Vinieratos <pvinis@gmail.com>
2018-12-31 15:34:53 +0100

    input button as a functional component

---
+++ PlaygroundApp/src/components/InputButtonFunc.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+    Text,
+    TouchableHighlight,
+    StyleSheet,
+} from 'react-native'
+
+const InputButton = (props) => {
+    const neutralColor = '#000'
+    const highlightColor = '#f77b2e'
+
+    const buttonColor = props.inverted
+        ? neutralColor
+        : highlightColor
+    const textColor = props.inverted
+        ? highlightColor
+        : neutralColor
+
+    return (
+        <TouchableHighlight
+            style={[styles.button, { backgroundColor: buttonColor }]}
+            onPress={props.onPress}
+        >
+            <Text style={[styles.text, { color: textColor }]}>
+                {props.value}
+            </Text>
+        </TouchableHighlight>
+    )
+}
+InputButton.defaultProps = {
+    inverted: false,
+}
+export default InputButton
+
+const styles = StyleSheet.create({
+    button: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 20,
+    },
+})
