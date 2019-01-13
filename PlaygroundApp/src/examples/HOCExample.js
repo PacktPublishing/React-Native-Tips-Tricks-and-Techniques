e6247dd12ba7a9db466ba368915d6cdcbd4cca87
Pavlos Vinieratos <pvinis@gmail.com>
2019-01-13 12:32:07 +0100

    start hoc example

---
+++ PlaygroundApp/src/examples/HOCExample.js
@@ -0,0 +1,19 @@
+// HOCs
+
+// Should
+// Pass on unneeded props
+// Change the displayName
+// Hoist static methods `hoist-non-react-statics`
+
+// Can
+// Have extra state
+// Manipulate props before passing on
+// Be as simple as logging props/information
+
+
+
+
+
+
+
+
