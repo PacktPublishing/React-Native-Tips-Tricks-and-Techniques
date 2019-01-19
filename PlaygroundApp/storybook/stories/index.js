5b96a92bb957ff24ca57c9fb40d8fb87ba110b66
Pavlos Vinieratos <pvinis@gmail.com>
2019-01-20 00:05:21 +0100

    some more examples

--- PlaygroundApp/storybook/stories/index.js
+++ PlaygroundApp/storybook/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-import { storiesOf } from '@storybook/react-native'
+import { storiesOf, addDecorator } from '@storybook/react-native'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
@@ -9,17 +9,31 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
 import CenterView from './CenterView'
 
 import InputButton from '../../src/components/InputButton'
+import withLongPressZoom from '../../src/components/withLongPressZoom'
 
-const stories = storiesOf('Storybook Knobs', module)
-stories.addDecorator(withKnobs)
+const InputButtonZoom = withLongPressZoom(InputButton)
 
 
-stories
+addDecorator(withKnobs)
+
+storiesOf('InputButton', module)
 .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
 .add('regular', () => (
   <InputButton
     value={3}
     inverted={boolean('inverted', false)}
     onPress={action('button clicked')}
+    onLongPress={action('button clicked looong')}
+  />
+))
+
+storiesOf('InputButtonZoom', module)
+.addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+.add('regular', () => (
+  <InputButtonZoom
+    value={3}
+    inverted={boolean('inverted', false)}
+    onPress={action('button clicked')}
+    onLongPress={boolean('has long press', true) ? action('button clicked looong') : undefined}
   />
 ))
