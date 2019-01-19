6130c6096f062121bb40ec7ff4407913077660b0
Pavlos Vinieratos <pvinis@gmail.com>
2019-01-19 22:38:39 +0100

    add knob

--- PlaygroundApp/storybook/stories/index.js
+++ PlaygroundApp/storybook/stories/index.js
@@ -1,37 +1,24 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
 
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react-native'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
 
-import Button from './Button';
-import CenterView from './CenterView';
-import Welcome from './Welcome';
+import CenterView from './CenterView'
 
 import InputButton from '../../src/components/InputButton'
 
+const stories = storiesOf('Storybook Knobs', module)
+stories.addDecorator(withKnobs)
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
-      <Text>Hello Button</Text>
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
-  ));
-
-storiesOf('InputButton', module)
+stories
 .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-.add('normal', () => (
-  <InputButton value={3} />
-))
-.add('inverted', () => (
-  <InputButton value={3} inverted />
+.add('regular', () => (
+  <InputButton
+    value={3}
+    inverted={boolean('inverted', false)}
+  />
 ))
